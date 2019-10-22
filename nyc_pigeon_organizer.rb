aa2f3b7368f3c9077f10b6ba3e9f96d7c0d73635
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-22 03:03:39 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pp'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    
+    new = Hash.new(0)
+    return new
+    
+end
+def nyc_pigeon_organizer(data)
+    new = Hash.new(0)
+    
+    data.keys
+      if key.size == 7
+       return new['key']
+        
+      end
+    
+    
 end
