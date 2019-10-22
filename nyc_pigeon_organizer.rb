c9ba7b78c8304deb1cdba0cbe799033e9413607a
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-22 03:15:11 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    new = Hash.new
+    
+    data.each do |key, value|
+      value.each do |att,names|
+        names.each do |name|
+          if !new.has_key?(name)
+            new[name] = {}
+          end
+          if !new[name].has_key?(key)
+             new[name][key] = []
+          end
+           if !new[name][key].include?(att)
+             new[name][key] << att.to_s
+           end
+           #binding.pry
+        end
+      end
+    end
+    return new
 end
